061fbb7bd3cd2d0aa1de159f50c3020e4215c4c0
dioncdz <dioncadizjr@gmail.com>
2020-10-04 21:04:23 +0800

    fixed functions

--- script.js
+++ script.js
@@ -1,117 +1,98 @@
-// ELEMENTS
-const cells = document.querySelectorAll('[data-cell]');
-const restartButton = document.querySelector('.restart');
-const newGame = document.querySelector('.new-game');
-let currentPlayer = true;
 
-const winningCombos = [
-   [true, true, true, 
-   false, false, false, 
-   false, false, false],
-
-   [false, false, false, 
-   true, true, true, 
-   false, false, false],
-
-   [false, false, false, 
-   false, false, false,
-   true, true, true],
-
-   [true, false, false, 
-   true, false, false,
-   true, false, false],
-
-   [false, true, false, 
-   false, true, false,
-   false, true, false],
-
-   [false, false, true, 
-   false, false, true,
-   false, false, true],
-
-   [true, false, false, 
-   false, true, false,
-   false, false, true],
-
-   [false, false, true, 
-   false, true, false,
-   true, false, false],
-]
-
-// FUNCTIONS
-function init() {
-   cells.forEach(el => {
-      el.classList.remove('x');
-      el.classList.remove('circle');
-      newGame.classList.remove('show')
-      // console.dir(el.classList);
-   })
-}
-
-function startGame() {
-   newGame.classList.add('show')
-}
-
-function arrayEquals (arr1, arr2) {
-   return JSON.stringify(arr1) === JSON.stringify(arr2)
-}
-
-function checkWinner() {
-   // make new arrays of X and Os
-   let xArr = []
-   let circleArr = []
+let setBoard = (function() {
+   // ELEMENTS
+   const cells = document.querySelectorAll('[data-cell]');
+   const restartButton = document.querySelector('.restart');
+   const newGame = document.querySelector('.new-game');
+   let winner = document.querySelector('.result')
+   let currentPlayer = true;
    
-   // make arrays of true/false combination
-   cells.forEach(el => {
-      if(currentPlayer) {
-         circleArr.push(el.classList.contains('circle'))
-      } else if (!currentPlayer) {
-         xArr.push(el.classList.contains('x'))
+   const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+   ]
+   
+   // FUNCTIONS
+   function init() {
+      cells.forEach(el => {
+         el.classList.remove('x');
+         el.classList.remove('circle');
+         newGame.classList.remove('show')
+      })
+   }
+   
+   function startGame() {
+      newGame.classList.add('show')
+      // TODO: create a computer opponent
+   }
+   
+   function declareWinner(player) {
+      winner.textContent = player.toUpperCase() + ' is the winner!'
+      startGame()
+   }
+   
+   function declareDraw() {
+      winner.textContent = "It's a draw!"
+      startGame()
+   }
+   
+   function checkWinner(playerTurn) {
+      let player;
+   
+      if(playerTurn) {
+         player = 'circle';
+      } else {
+         player = 'x';
       }
+   
+      let isWinner = winningCombos.some(combination => {
+         return combination.every(index => {
+            return [...cells][index].classList.contains(player)
+         })
+      })
+   
+      // If there's a winner declare a winner
+      if (isWinner) {
+         declareWinner(player)
+      }
+   
+      // If board is full and no winner === DRAW
+      if([...cells].every(cell => {return cell.classList.length > 1})) {
+         declareDraw()
+      }
+   
+   }
+   
+   function playerMark(e) {
+   
+      if(e.target.classList.length === 1) {
+         if(currentPlayer) {
+            e.target.classList.add('circle')
+         } else {
+            e.target.classList.add('x')
+         }
+   
+         checkWinner(currentPlayer);
+         currentPlayer = !currentPlayer;
+      }
+   
+   }
+
+   // EVENT LISTENERS
+   restartButton.addEventListener('click', init)
+   
+   cells.forEach(el => {
+      el.addEventListener('click', playerMark)
    })
 
-   // match x and o arrays with winning combinations
-   for (let combo of winningCombos) {
-      console.log(`${arrayEquals(combo, circleArr)}`)
-      if(
-         arrayEquals(combo, xArr) || 
-         arrayEquals(combo, circleArr)
-         ) {
-            // TODO: DECLARE WINNER
-            
-            startGame()
-            return currentPlayer ? `${currentPlayer} won!` : `${currentPlayer} won!`
-         }
-   }
+   return {startGame};
+})()
 
+setBoard.startGame()
 
-   if([...cells].every(el => el.classList.length > 1)) {
-      console.log("It's a draw");
-   }
-   console.log(xArr, circleArr);
-   // If no winner or no draw, reset array values
-   xArr = []
-   circleArr = []
-}
-
-function playerMark(e) {
-   if(currentPlayer) {
-      e.target.classList.add('circle')
-   } else {
-      e.target.classList.add('x')
-   }
-
-   checkWinner();
-   currentPlayer = !currentPlayer;
-}
-
-// init();
-startGame() 
-
-
-// EVENT LISTENERS
-restartButton.addEventListener('click', init)
-
-cells.forEach(el => {
-   el.addEventListener('click', playerMark, {once: true})
-})
